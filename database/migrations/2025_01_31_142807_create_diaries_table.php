1a06569ad6241a7688ceb100914c43b81b065f00
Yuto299 <ota.se.ke@outlook.jp>
2025-01-31 14:35:03 +0900

    9-

---
+++ database/migrations/2025_01_31_142807_create_diaries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('diaries', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('diaries');
+    }
+}; 
+
+//docker compose exec php-container php artisan make:migration create_diaries_table
